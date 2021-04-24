51ca8acbb27b90d64235fe9a15fe42707998f623
huanxin105 <66866489+huanxin105@users.noreply.github.com>
2021-04-24 18:09:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,79 +1,65 @@
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
-import math
+
+def entropy(s):    
+    _, counts = np.unique(list(s), return_counts=True)
+    total = sum(counts)
+    percent = list(map(lambda x: x / total, counts))
+    return sum(-n * np.log(n) for n in percent)
+    
+def processDomain(s):
+    return len(s), sum(c.isdigit() for c in s), entropy(s)
 
 class Domain:
-	def __init__(self,  _name, _label, _length, _num, _entropy):
-		self.name = _name
-		self.label = _label
-		self.length = _length
-		self.num = _num
-		self.entropy = _entropy
-	def returnData(self):
-		return [self.length, self.num, self.entropy]
-	def returnLabel(self):
-		if self.label == "notdga":
-			return 0
-		else:
-			return 1
+    def __init__(self, _domain, _label):
+        self.domain = _domain
+        self.label = _label
+        self.domainNameLength, self.domainNumberCount, self.letterEntropy = processDomain(self.domain)
+    
+    def returnData(self):
+        return [self.domainNameLength, self.domainNumberCount, self.letterEntropy]
 
-def calculateNum(str):
-        num = 0
-	for i in str:
-		if i.isdigit():
-			num += 1
-	return num
+    def returnLabel(self):
+        if self.label == "notdga":
+            return 0
+        else:
+            return 1
 
-def calculateEntropy(str):
-	#统计字符数目
-	result = {}
-	for i in str:
-		result[i] = str.count(i)
-	sum = len(str)
-	#计算熵值
-	entropy = 0
-	for j in result:
-		entropy = entropy - float(result[j] / sum) * math.log(float(result[j] / sum), 2)
-	return entropy
+domainList = []      
 
-def initData(filename, domainlist):
-	with open(filename) as f:
-		for line in f:
-			line = line.strip()
-			if line.startswith("#") or line == "":
-				continue
-			tokens = line.split(",")
-			name = tokens[0]
-			if len(tokens) > 1
-				label = tokens[1]
-			else
-				label = "unknown"
-			length = len(name)
-			num = calculateNum(name)
-			entropy = calculateEntropy(name)
-			domainlist.append(Domain(name, label, length, num, entropy))
-			
-def main():
-	#读取训练文件
-	domainlist1 = []
-	initData("train", domainlist1)
-	featureMatrix = []
-	labelList = []
-	for item1 in domainlist1:
-		featureMatrix.append(item1.returnData())
-		labelList.append(item1.returnLabel())
-	#训练
-	clf = RandomForestClassifier(random_state = 0)
-	clf.fit(featureMatrix, labelList)
-	#读取测试文件
-	domainlist2 = []
-	initData("test", domainlist2)
-	with open("result", 'w') as f:
-		for item2 in domainlist2
-			f.write(item2.name)
-			f.write(", ")
-			f.write(clf.predict([item2.returnData]))
-			f.write("\n")
-	
+def initData(filename):
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if line == "":
+                continue
+            tokens = line.split(",")
+            domain = tokens[0]
+            label = tokens[1]
+            domainList.append(Domain(domain, label))
+
+def initTest(filename):
+    with open(filename) as f:
+        return list(filter(lambda line: line != "", map(lambda line: line.strip(), f)))
+
 if __name__ == '__main__':
-	main()
+    initData("train.txt")
+    featureMatrix = list(map(lambda domain: domain.returnData(), domainList))
+    labelList = list(map(lambda domain: domain.returnLabel(), domainList))
+    testDomains = initTest("test.txt")
+    testDomainFeatures = []
+    for domain in testDomains:
+        domainNameLength, domainNumberCount, letterEntropy = processDomain(domain)
+        testDomainFeatures.append([domainNameLength, domainNumberCount, letterEntropy])
+    clf = RandomForestClassifier(random_state=0)
+    clf.fit(featureMatrix, labelList)
+    testLabels = clf.predict(testDomainFeatures)
+    output = list(zip(testDomains, testLabels))
+    with open("result.txt", "w+") as f:
+        for domain, label in output:
+            line = domain + ","
+            if label == 0:
+                line = line + "notdga\n"
+            else:
+                line = line + "dga\n"
+            f.write(line)
